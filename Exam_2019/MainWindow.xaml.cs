5de21c5def3cf6a9fd6630d7f2356797ce4295b7
Jake Smith <S00234729@atu.ie>
2023-12-08 14:25:43 +0000

    Q 5 + 6 Added random players Dob and positions

--- Exam_2019/MainWindow.xaml.cs
+++ Exam_2019/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace Exam_2019
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        List<Player> allPlayers = new List<Player>();
+        List<Player> selectedPlayers = new List<Player>();
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +36,67 @@ namespace Exam_2019
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
+        }
+
+        private List<Player> CreatePlayers()
+        {
+            List <Player> players = new List<Player>();
+            Random r = new Random();
+            string[] firstNames = {
+                "Adam", "Amelia", "Ava", "Chloe", "Conor", "Daniel", "Emily",
+                "Emma", "Grace", "Hannah", "Harry", "Jack", "James",
+                "Lucy", "Luke", "Mia", "Michael", "Noah", "Sean", "Sophie"};
+            string[] lastNames = {
+                "Brennan", "Byrne", "Daly", "Doyle", "Dunne", "Fitzgerald", "Kavanagh",
+                "Kelly", "Lynch", "McCarthy", "McDonagh", "Murphy", "Nolan", "O'Brien",
+                "O'Connor", "O'Neill", "O'Reilly", "O'Sullivan", "Ryan", "Walsh"
+            };
+
+            Position currentPosition = Position.Goalkeeper;
+            //Create 18 players
+            for (int i = 0; i < 18; i++)
+            {
+
+                DateTime date1 = DateTime.Now.AddYears(-30);
+                DateTime date2 = DateTime.Now.AddYears(-20);
+                TimeSpan t = date2 - date1;
+                int numberOfDays = t.Days;
+                DateTime newDate = date1.AddDays(r.Next(numberOfDays));
+
+                
+
+                switch(i)
+                {
+                    case 2:
+                        currentPosition = Position.Defender;
+                        break;
+                    case 8:
+                        currentPosition = Position.Midfielder;
+                        break;
+                    case 14:
+                        currentPosition = Position.Forward;
+                        break;
+                }
+
+
+                Player p = new Player()
+                {
+                    FirstName = firstNames[r.Next(20)],
+                    SurName = lastNames[r.Next(20)],
+                    DateOfBirth = newDate,
+                    PreferedPosition = currentPosition
+                };
+
+                players.Add(p); 
+            }
+            return players;
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            allPlayers = CreatePlayers();
+
+            lbxAllPlayers.ItemsSource = allPlayers;
         }
     }
 }
