17a9e4ce063316615e601966775c6615e33aaf77
Jake Smith <S00234729@atu.ie>
2023-12-08 14:01:55 +0000

    Q 3 + 4 created class and enum

---
+++ Exam_2019/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_2019
+{
+
+    public enum Position { Goalkeeper, Defender, Midfielder, Forward}
+    public class Player
+    {
+        public string FirstName { get; set; }
+
+        public string SurName { get; set; }
+
+        public Position PreferedPosition { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        private int age;
+
+        public int Age
+        {
+            get 
+            {
+                age = DateTime.Now.Year - DateOfBirth.Year;
+                if (DateOfBirth.DayOfYear >= DateTime.Now.DayOfYear)
+                    age--;
+                return age;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName}{SurName}({Age}){PreferedPosition.ToString().ToUpper()}";
+        }
+    }
+}
